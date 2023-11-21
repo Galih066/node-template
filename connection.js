d7da83f075400119986b24782316562be3987bfb
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2023-11-21 21:17:46 +0800

    Add: Connection to database file

--- connection.js
+++ connection.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose")
+
+const { DB_URL } = process.env
+
+exports.connect = () => {
+    mongoose.connect(DB_URL)
+        .then(() => console.info("Database connection established"))
+        .catch(err => {
+            console.error("Database connection failure", err)
+            process.exit(1)
+        })
+}
